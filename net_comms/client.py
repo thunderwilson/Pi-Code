318202a657fd764513a3c527a881c52f8acaf73b
Tom Wilson <thomas.wilson983@gmail.com>
2014-12-09 21:27:19 +0000

    Differentiating server and client side code

--- net_comms/client.py
+++ net_comms/client.py
@@ -1,10 +1,9 @@
 # Echo client program
-..    import socket
-.
-    HOST = '192.168.20.113' #remote host
-    PORT = 50007              # The same port as used by the server
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
-    s.sendall('Hello, world')
-    data = s.recv(1024)
-    s.close
+import socket
+HOST = '192.168.20.116'
+PORT = 50007  # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall('Hello, world')
+data = s.recv(1024)
+s.close
