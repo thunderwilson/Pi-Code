1239903e6537fcbd6ace289d4aeeac6fb17a9267
Tom Wilson <thomas.wilson983@gmail.com>
2015-01-12 06:52:41 +0000

    Old shit

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-hciconfig hci0 down
-hciconfig hci1 leadv 3
-hciconfig hci1 noscan
+hciconfig hci0 up
+hciconfig hci0 leadv 3
+hciconfig hci0 noscan
 
 
 hcitool -i hci0 cmd 0x08 0x0008 1E 02 01 1A 1A FF 4C 00 02 15 63 6F 3F 8F 64 91 4B EE 95 F7 D8 CC 64 A8 63 B5 00 00 00 00 C8
